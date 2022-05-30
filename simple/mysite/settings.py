34d381c9be05c5b80f33157ec8bf058b750dcb66
RajAnubhav <anubhavraj742@gmail.com>
2022-05-31 00:48:19 +0530

    pushing for heroku deployment

--- simple/mysite/settings.py
+++ simple/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(d2$b2pqj7i+ws!n+iby=k&($68hkarz%_@&0z6n*t_$nr$=d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'arnotesmaker.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
